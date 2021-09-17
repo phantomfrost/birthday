46024e2d78620ab3576be1ce7678c9c797ae949a
phantomfrost <78469955+phantomfrost@users.noreply.github.com>
2021-09-17 18:39:53 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
   //修改密码请改此处
-  if (userName == "薛慧" && pwd == "123456") {
+  if (userName == "刘海峰" && pwd == "123456") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
